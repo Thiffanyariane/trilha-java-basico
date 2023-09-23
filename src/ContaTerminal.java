4ff0f3eee3ab303b6769683ed952ff4b4e281504
Thiffany <thiffany.ariane@veri.com.br>
2023-09-22 23:06:42 -0300

    Meu projeto Conta Bancária.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
-    public static void main(String[] args){
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da conta! ");
+        int Numero = scanner.nextInt();
+
+        System.out.println("Digite o número da agência! ");
+        String Agencia = scanner.next();
+
+        System.out.println("Digite seu nome! ");
+        String NomeCliente = scanner.next().concat(" " ).concat(scanner.next());
+
+        System.out.println("Digite o saldo! ");
+        double Saldo = scanner.nextDouble();
+
+        System.out.println("Olá ".concat(NomeCliente).concat(
+                ", obrigado por criar uma conta em nosso banco, sua agência é ").concat(Agencia).concat(
+                        ", conta " ).concat(Integer.toString(Numero)).concat(" e seu saldo " ).concat(
+                                Double.toString(Saldo)).concat(" já está disponível para saque."));
+
     }
 }
